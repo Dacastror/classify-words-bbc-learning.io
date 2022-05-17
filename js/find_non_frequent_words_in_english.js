7d874aefbde2adc470574edaf1a3d66735ad2dfc
Diego Alberto Castro Rodriguez <dacastror@gmail.com>
2022-05-17 03:42:39 -0500

    creation of basic operating functions

---
+++ js/find_non_frequent_words_in_english.js
@@ -0,0 +1,48 @@
+function getWordsByFrequecy(corpus, input_string) {
+    const input_words_list = cleanInput(input_string)
+    const pairs = pairsWordIndex(corpus, input_words_list)
+    return sortWords(pairs)
+}
+
+function cleanInput(string) {
+    let new_str = " " + string + " "
+    new_str = new_str.toLowerCase()
+    new_str = new_str.replace(/-\n +/g, "")
+    new_str = new_str.replace(/-\n/g, "")
+    new_str = new_str.replace(/(\n-|\n)/g, " ")
+    new_str = new_str.replace(/can('|‘|’)t[^\p{L}]/gu, "can not ")
+    new_str = new_str.replace(/i('|‘|’)m[^\p{L}]/gu, " i am ")
+    new_str = new_str.replace(/n('|‘|’)t[^\p{L}]/gu, " not ")
+    new_str = new_str.replace(/('|‘|’)t[^\p{L}]/gu, " not ")
+    new_str = new_str.replace(/('|‘|’)(ll|re|ve|d|s)[^\p{L}]/gu, " ")
+    new_str = new_str.replace(/[^\p{L} -]/gu, " ") // remover caracteres que no son letras
+    new_str = new_str.replace(/( -|- )/g, " ")
+    new_str = new_str.replace(/  +/g, " ")
+    new_str = new_str.trim()
+    let output_array = new_str.split(" ")
+    output_array = [...new Set(output_array)] // remover palabras repetidas
+    return output_array
+}
+
+function pairsWordIndex(corpus, input_words_list) {
+    let pairs = []; let word = ""; let indice;
+    for (let i = 0; i < input_words_list.length; i++) {
+        word = input_words_list[i]
+        indice = corpus.indexOf(word)
+        if (indice > -1) {
+            pairs.push([word, corpus.indexOf(word)])
+        }
+    }
+    return pairs
+}
+
+function sortWords(pairs) {
+    pairs.sort(function (first, second) {
+        return second[1] - first[1];
+    })
+    let sorted_words = []
+    for (let i = 0; i < pairs.length; i++) {
+        sorted_words.push(pairs[i][0])
+    }
+    return sorted_words
+}
---
+++ js/main.js
@@ -0,0 +1,128 @@
+const ID_SEARCH_BUTTON = "search_btn"
+const ID_HOME_BUTTON = "home-bt"
+const ID_INPUT_START_TIME = "time_ini"
+const ID_INPUT_END_TIME = "time_fin"
+const ID_DIV_DICTATIONS = "dictations"
+const ID_INTERFACE_SEARCH = "interface_buscar_palabras"
+const ID_HARD_WORD_LIST = "list_hard_words"
+const ID_MEDIUM_WORD_LIST = "list_medium_words"
+const ID_EASY_WORD_LIST = "list_easy_words"
+const ID_RESULTS = "results"
+const CLASS_DICTATE = "dictate"
+const VIDEO_SELECTED = "video_selected"
+
+addEventLiteners()
+
+function addEventLiteners(){
+    let search_btn = document.getElementById(ID_SEARCH_BUTTON)
+    search_btn.addEventListener("click", buildAnswer);
+    let home_btn = document.getElementById(ID_HOME_BUTTON)
+    home_btn.addEventListener("click", makeHome);
+    let lecciones = document.getElementsByClassName(CLASS_DICTATE)
+    for(let i=0; i<lecciones.length; i++){
+        let img = document.getElementById(String(i+1))
+        img.addEventListener("click", accionesClickImagen);
+    }
+}
+
+function timeToSeconds(string){
+    let l = string.split(":")
+    return parseFloat(l[0])*60 + parseFloat(l[1])
+}
+
+function getStartAndStopTime(){
+    let time_ini = document.getElementById(ID_INPUT_START_TIME).value
+    let time_fin = document.getElementById(ID_INPUT_END_TIME).value
+    time_ini = timeToSeconds(time_ini)
+    time_fin = timeToSeconds(time_fin)
+    return {time_ini, time_fin};
+}
+
+function searchIndexStartEnd(times_words,time_ini, time_fin){
+    let start_index = 0
+    for (let i=0; i<times_words.length; i++){
+        if (times_words[i][1] > time_ini){
+            start_index = i
+            break
+        } 
+    }
+    let end_index = 0
+    for (let i=0; i<times_words.length; i++){
+        if (times_words[i][0] > time_fin){
+            end_index = i
+            break
+        } 
+    }
+    return {start_index, end_index};
+}
+
+function selectText(times_words,start_index,end_index){
+    let selected_text = ""
+    for (let i = start_index; i<end_index; i++){
+        selected_text = selected_text + " " + times_words[i][2] 
+    }
+    return selected_text
+}
+
+function buildItem(num, word){
+    let w = '<h6>' + word + '</h6>'
+    let td_num = '<td align="left"><h6>' + num + '</h6></td>\n'
+    let td_word = '<td width="91%" align="left" valign="middle" height="35">' + w + '</td>\n'
+    return  '<tr>\n' + td_num + td_word + '</tr>\n'
+}
+
+function buildItems(words,id,num_ini){
+    let items = ""
+    for (let i=0;i<words.length;i++){
+        items = items + buildItem(i+1+num_ini, words[i])
+    }
+    let word_list_element = document.getElementById(id)
+    word_list_element.innerHTML = items
+}
+
+
+function splitlist(array, parts) {
+    let result = [];
+    for (let i = parts; i > 0; i--) {
+        result.push(array.splice(0, Math.ceil(array.length / i)));
+    }
+    return result;
+}
+
+function capitalized(list_words){
+    let result = []
+    for (let i=0; i<list_words.length; i++){
+        result.push(list_words[i].charAt(0).toUpperCase() + list_words[i].slice(1))
+    }
+    return result
+}
+
+function buildAnswer(){
+    let num_video = sessionStorage.getItem(VIDEO_SELECTED)
+    let times_words = subs[num_video]
+    let {time_ini, time_fin} = getStartAndStopTime()
+    let {start_index, end_index} = searchIndexStartEnd(times_words,time_ini,time_fin)
+    let input_string = selectText(times_words,start_index,end_index)
+    let words_by_freq = getWordsByFrequecy(corpus, input_string)
+    words_by_freq = capitalized(words_by_freq)
+    let word_groups = splitlist(words_by_freq, 3)
+    buildItems(word_groups[0],ID_HARD_WORD_LIST,0)
+    let num_ini = word_groups[0].length
+    buildItems(word_groups[1],ID_MEDIUM_WORD_LIST,num_ini)
+    let num_ini2 = num_ini + word_groups[1].length
+    buildItems(word_groups[2],ID_EASY_WORD_LIST, num_ini2)
+    document.getElementById(ID_RESULTS).style.display = 'block';
+}
+
+function makeHome(){
+    document.getElementById(ID_DIV_DICTATIONS).style.display = 'block';
+    document.getElementById(ID_RESULTS).style.display = 'none';
+    document.getElementById(ID_INTERFACE_SEARCH).style.display = 'none';
+}
+
+function accionesClickImagen(){
+    let num_video = parseFloat(this.id)
+    sessionStorage.setItem(VIDEO_SELECTED, num_video)
+    document.getElementById(ID_DIV_DICTATIONS).style.display = 'none';
+    document.getElementById(ID_INTERFACE_SEARCH).style.display = 'block';
+}
